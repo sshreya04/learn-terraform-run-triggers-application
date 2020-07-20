4472b9628c9a4981284c19d80adb93d1384b6caf
sshreya04 <38071494+sshreya04@users.noreply.github.com>
2020-07-20 07:14:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable tfc_org_name {
   description = "Name of the Terraform Cloud Organization"
   type        = string
-  default     = "hashicorp-learn"
+  default     = "shreya-org"
 }
 
 variable tfc_network_workspace_name {
